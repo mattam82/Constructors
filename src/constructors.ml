802944ff0e0afa095b7b726e74c317357c34ac8e
Matthieu Sozeau <mattam@mattam.org>
2010-05-31 11:07:08 +0200

    Proper license and copyright information.

--- src/constructors.ml
+++ src/constructors.ml
@@ -1,10 +1,8 @@
-(************************************************************************)
-(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
-(* <O___,, * CNRS-Ecole Polytechnique-INRIA Futurs-Universite Paris Sud *)
-(*   \VV/  **************************************************************)
-(*    //   *      This file is distributed under the terms of the       *)
-(*         *       GNU Lesser General Public License Version 2.1        *)
-(************************************************************************)
+(***********************************************************)
+(* Copyright 2010 Matthieu Sozeau <mattam@mattam.org>      *)
+(* This file is distributed under the terms of the         *)
+(* GNU Lesser General Public License Version 2.1           *)
+(***********************************************************)
 
 (** Constructors - An example plugin and tactic for Coq
 
